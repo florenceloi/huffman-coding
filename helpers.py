88d022eede58feee0392ce9c2ba70d0bd75145a0
Florence Loi <florence.c.loi@gmail.com>
2017-04-16 21:29:46 -0700

    Finalize Huffman code tree

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@
 
 from collections import deque
 
+
 class CustomOpen(object):
 
     def __init__(self, filename):
@@ -17,36 +18,9 @@ class CustomOpen(object):
 class CharTree(object):
 
     def __init__(self, freq, char, left=None, right=None):
-        self.freq = freq
-        self.char = char
+        self.data = (freq, char)
         self.left = left
         self.right = right
 
-    # def __repr__(self):
-    #     nodes = deque()
-    #     levels = deque()
-
-    #     nodes.append(self)
-    #     level = 1
-    #     levels.append(level)
-    #     print level, "(%s: %d)" % (self.char, self.freq)
-
-    #     while len(nodes) > 0:
-    #         node = nodes.popleft()
-    #         level = levels.popleft()
-
-    #         if node.left is not None:
-    #             nodes.append(node.left)
-    #             levels.append(level + 1)
-
-    #         if node.right is not None:
-    #             nodes.append(node.right)
-    #             levels.append(level + 1)
-
-    #         if len(levels) > 0 and levels[0] > level and levels[0] == levels[-1]:
-    #             level += 1
-    #             for item in nodes:
-    #                 print level, "(%s: %d)" % (self.char, self.freq)
-
     def __cmp__(self, other):
-        return cmp(self.freq, other.freq)
+        return cmp(self.data[0], other.data[0])
--- huffman_coding.py
+++ huffman_coding.py
@@ -6,43 +6,40 @@ from helpers import CustomOpen, CharTree
 
 def create_char_freq_map(filename):
 
-    char_frequency = {}
+    char_frequency_dict = {}
 
     with CustomOpen(filename) as f:
         contents = f.read()
         for char in contents:
-            value = char_frequency.get(char, 0)
-            char_frequency[char] = value + 1
+            value = char_frequency_dict.get(char, 0)
+            char_frequency_dict[char] = value + 1
 
-    return char_frequency
+    return char_frequency_dict
 
 
 def create_priority_queue(char_dict):
 
-    char_priority = PriorityQueue()
+    char_priority_queue_dict = PriorityQueue()
 
-    for k, v in char_dict.iteritems():
-        char_priority.put(CharTree(v, k))
+    for char, frequency in char_dict.iteritems():
+        char_priority_queue_dict.put((frequency, CharTree(frequency, char)))
 
-    return char_priority
+    return char_priority_queue_dict
 
 
 def create_priority_binary_tree(priority_queue):
 
     while priority_queue.qsize() > 1:
-        print priority_queue.qsize()
-        left = priority_queue.get()
-        right = priority_queue.get()
-        freq = left.freq + right.freq
-        root = CharTree(freq, None, left, right)
-        print "Combining %s (%d) and %s (%d) to add %d" % (left.char,
-                                                           left.freq,
-                                                           right.char,
-                                                           right.freq,
-                                                           root.freq)
-        priority_queue.put(root)
+        print 'Current priority queue size: {num}'.format(num=priority_queue.qsize())
+        tree_one = priority_queue.get()
+        tree_two = priority_queue.get()
+        total_freq = tree_one[0] + tree_two[0]
+        print '\t-> total frequency: {freq}'.format(freq=total_freq)
+        root = CharTree(total_freq, None, tree_one, tree_two)
+        priority_queue.put((total_freq, root))
 
-    return priority_queue.get()
+    print '\nFinal length of priority queue: {length}'.format(length=priority_queue.qsize())
+    return priority_queue
 
 
 # def create_char_code_map(priority_binary_tree):
@@ -52,10 +49,8 @@ def create_priority_binary_tree(priority_queue):
 if __name__ == '__main__':
 
     char_frequency = create_char_freq_map('samples/example.txt')
-    char_priority = create_priority_queue(char_frequency)
-    char_tree = create_priority_binary_tree(char_priority)
-
-    print char_tree
+    char_priority_queue = create_priority_queue(char_frequency)
+    char_tree = create_priority_binary_tree(char_priority_queue)
 
 
 ##############################################################################
@@ -63,7 +58,7 @@ if __name__ == '__main__':
 
 # - Generate Frequency Table
 # - Put Singleton Trees in Priority Queue
-# - Tree Creation   --> NEED TO FINISH __REPR__ FOR CHAR_TREE
-# - Code Retrieval
+# - Tree Creation
+# - Code Retrieval --> NEED TO CONSTRUCT MAP USING SINGLE TRAVERSAL OF HUFFMAN CODE TREE
 # - Compression
 # - Decompression
